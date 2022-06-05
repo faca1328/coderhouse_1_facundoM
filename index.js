89d2b7432f27234717accb7a94f36fa97967f7e5
Facu M <facundoivanmartinez@hotmail.com>
2022-06-05 20:00:10 -0300

    arreglo menu hamburguesa y comienzo responsive para mobile

--- index.js
+++
@@ -1,6 +0,0 @@
-const header__boton_desplegable = document.querySelector( ".header__boton_desplegable" )
-const header__navegador = document.querySelector( ".header__navegador" )
-
-header__boton_desplegable.addEventListener( "click", () => {
-    header__navegador.classList.toggle("header__navegador--visible");
-})
